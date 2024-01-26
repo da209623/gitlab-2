3501b9de478c1aa6fa3e257d554eb78e0e12b653
da209623 <143651968+da209623@users.noreply.github.com>
2024-01-26 14:05:10 -0500

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/* 
+* File: git-lab-program.cc
+* Author: Dustin Adams
+* Date: 1/24/24
+* Description: Intro Github program.
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+
